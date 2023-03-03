228c732d4bc71506355ed278ff08bd339fe5ad3c
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-03 10:15:47 +0300

    movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,43 +1,45 @@
 import React, { useContext } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { AuthContext } from "../context/AuthContextProvider";
-// import { toastWarnNotify } from "../helpers/ToastNotify";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContextProvider";
+import { toastWarnNotify } from "../helpers/ToastNotify";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-const MovieCard = ({movie}) => 
-{
-  //   const { currentUser } = useContext(AuthContext);
-  //   const navigate = useNavigate();
+const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  //   const getVoteClass = (vote) => {
-  //     if (vote >= 8) {
-  //       return "green";
-  //     } else if (vote >= 6) {
-  //       return "orange";
-  //     } else {
-  //       return "red";
-  //     }
-  //   };
-  const {poster_path, title, overview, vote_average, id, vote_count} = movie
+  const getVoteClass = (vote) => {
+    if (vote >= 8) {
+      return "green";
+    } else if (vote >= 6) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  };
   return (
     <div
       className="movie"
-      //   onClick={() => {
-      //     navigate("details/" + id);
-      //     !currentUser && toastWarnNotify("please log in to see details");
-      //   }}
+      onClick={() => {
+        navigate("details/" + id);
+        !currentUser && toastWarnNotify("please log in to see details");
+      }}
     >
-      <img loading="lazy" src={poster_path} alt="movie-card" />
+      <img
+        loading="lazy"
+        src={poster_path ? IMG_API + poster_path : defaultImage}
+        alt="movie-card"
+      />
       <div className="flex align-baseline justify-between p-1 text-white">
-        <h5>sdfg</h5>
-        {/* {currentUser && (
+        <h5>{title}</h5>
+        {currentUser && (
           <span className={`tag ${getVoteClass(vote_average)}`}>
             {vote_average.toFixed(1)}
           </span>
-        )} */}
+        )}
       </div>
       <div className="movie-over">
         <h2>Overview</h2>
