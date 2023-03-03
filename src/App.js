97746fc1b4d4a005cea194f591576454c8b5d250
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-03 10:01:16 +0300

    toastify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-// import { ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+
 import AuthContextProvider from "./context/AuthContextProvider";
 import AppRouter from "./router/AppRouter";
 
@@ -8,7 +9,7 @@ const App = () => {
     <div className="dark:bg-[#23242a]">
     <AuthContextProvider>
       <AppRouter />
-      {/* <ToastContainer /> */}
+      <ToastContainer />
     </AuthContextProvider>
      </div>
   );
