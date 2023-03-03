f386f0568afc8286faf9315846914d1c3fb1b06d
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-03 10:12:23 +0300

    appRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import {BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "../pages/Main";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import MovieDetail from "../pages/MovieDetail";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-        <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/details/:id" element={<MovieDetail/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<PrivateRouter />}>
+          <Route path="" element={<MovieDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
