73a977a1b3ceaeb935216a6fa086e56160d94217
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-26 23:03:37 +0300

    register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,24 +4,17 @@ import GoogleIcon from "../assets/icons/GoogleIcon";
 import { createUser, signUpWithGoogle } from "../auth/firebase";
 
 const Register = () => {
-  //* ayrı stateler
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  //* birleştirilmiş state
-  // const [info, setInfo] = useState({
-  //   firstName: "",
-  //   lastName: "",
-  //   email: "",
-  //   password: "",
-  // });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const displayName = `${firstName} ${lastName}`;
     createUser(email, password, navigate, displayName);
+
     console.log(firstName, lastName);
   };
 
@@ -29,24 +22,21 @@ const Register = () => {
     signUpWithGoogle(navigate);
   };
 
-  // const hadleChange = (e) =>
-  //   setInfo({ ...info, [e.target.id]: e.target.value });
-
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center ">
       <div className="form-image hidden md:block">
         <img
           src="https://picsum.photos/800/800"
           alt="sample-movie"
-          className="object-cover h-screen w-full"
+          className="object-cover h-screen w-full "
         />
       </div>
-      <div className="overflow-hidden flex-1 h-screen justify-center items-center dark:bg-[#23242a]">
+      <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
         <div
-          className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-gray-100 dark:bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
+          className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
         >
           <form
-            className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] form flex flex-col p-20"
+            className="absolute inset-[2px] rounded-[8px] bg-[#28292d] z-[10] form flex flex-col p-20"
             onSubmit={handleSubmit}
           >
             <h2 className="text-[#ff4b45] text-2xl font-[500] text-center tracking-[0.1em]">
